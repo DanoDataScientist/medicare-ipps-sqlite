20eeee34b96b0fab4886043dc7708521f2d50b4d
jlrsource <jlrsource@users.noreply.github.com>
2016-01-13 21:20:23 -0600

    Defined dataDir variable in cmsdata.py

--- cmsdata.py
+++ cmsdata.py
@@ -11,15 +11,16 @@ dataRoot = ('https://www.cms.gov/' +
 data2011 = 'Inpatient_Data_2011_CSV.zip'
 data2012 = 'Inpatient_Data_2012_CSV.zip'
 data2013 = 'Inpatient_Data_2013_CSV.zip'
+dataDir = 'data/'
 
 def retrieve():
     """Retrieve Medicare data files from cms.gov website.
     """
     print 'Retrieving Medicare data...'
-    urllib.urlretrieve(dataRoot+data2011, 'data/'+data2011)
+    urllib.urlretrieve(dataRoot+data2011, dataDir+data2011)
     print '2011 Medicare data retrieved.'
-    urllib.urlretrieve(dataRoot+data2012, 'data/'+data2012)
+    urllib.urlretrieve(dataRoot+data2012, dataDir+data2012)
     print '2012 Medicare data retrieved.'
-    urllib.urlretrieve(dataRoot+data2013, 'data/'+data2013)
+    urllib.urlretrieve(dataRoot+data2013, dataDir+data2013)
     print '2013 Medicare data retrieved.'
 
